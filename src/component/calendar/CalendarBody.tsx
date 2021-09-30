2b14d77826feb414f700155446e74ae15a93d51c
chocoyam <440709@naver.com>
2021-09-30 10:36:09 +0900

    [feat]CalendarBody를 Card 컴포넌트로 변경 - material-ui의 card로 하루를 표현하는 Day 컴포넌트 추가 - Day 컴포넌트의 prop으로 사용할 DayProps 정의

--- src/component/calendar/CalendarBody.tsx
+++ src/component/calendar/CalendarBody.tsx
@@ -1,15 +1,31 @@
 import React from 'react';
+import { Card } from '@material-ui/core';
+import { Typography } from '@material-ui/core';
+
+interface DayProps {
+    dayNumber? : number,
+    schedules? : string[]
+}
+
+const Day: React.FC<DayProps> = ({dayNumber, schedules}: DayProps) => {
+    return (
+        <Card>
+        <Typography variant="h6">{dayNumber}</Typography>
+        <Typography variant="body1">{schedules}</Typography>
+        </Card>
+    );
+};
 
 function CalendarBody() {
     return (
         <tr>
-            <td>1</td>
-            <td>2</td>
-            <td>3</td>
-            <td>4</td>
-            <td>5</td>
-            <td>6</td>
-            <td>7</td>
+            <td><Day dayNumber={1} schedules={[]}></Day></td>
+            <td><Day dayNumber={2} schedules={[]}></Day></td>
+            <td><Day dayNumber={3} schedules={[]}></Day></td>
+            <td><Day dayNumber={4} schedules={[]}></Day></td>
+            <td><Day dayNumber={5} schedules={[]}></Day></td>
+            <td><Day dayNumber={6} schedules={[]}></Day></td>
+            <td><Day dayNumber={7} schedules={[]}></Day></td>
         </tr>
     );
 };
