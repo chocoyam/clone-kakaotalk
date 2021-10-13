8f19863babf68f16818776a308172fded3fc8bbd
chocoyam <440709@naver.com>
2021-10-13 18:34:47 +0900

    [feat]ScheduleStore 생성 - 캘린더에 표시할 날짜와 일정 정보를 관리하는 스토어 - 기존 ScheduleModel 구현부를 스토어에 포함시킴 - 캘린더에 표시할 날짜는 DayModel, 일정 정보는 ScheduleModel의 interface로 표현 - DayModel이 ScheduleModel을 포함하는 관계

---
+++ src/store/ScheduleStore.ts
@@ -0,0 +1,55 @@
+import { observable, action, computed } from 'mobx';
+import ScheduleModel from './ScheduleModel';
+
+interface DayModel {
+    date: Date;
+    schedules: ScheduleModel[];
+}
+
+interface ScheduleModel {
+    subject: string;
+    startDate: Date;
+    endDate: Date;
+    memo: string;
+    get getSubject(): string;
+    get getStartDate(): Date;
+    get getEndDate(): Date;
+    get getMemo(): string;
+}
+
+class ScheduleStore {
+    private dayModels: DayModel[];
+
+    constructor() {
+        this.dayModels = [];
+    }
+
+    public makeDayModel(date: Date) {
+        const schedules: ScheduleModel[] = [];
+        const newDay: DayModel = {date, schedules};
+        this.dayModels.push(newDay);
+    }
+
+    public makeSchedule(dayID: number, subject: string, startDate: Date, endDate: Date, memo: string) {
+        const newSchedule: ScheduleModel = observable(
+            {
+                subject, startDate, endDate, memo,
+                get getSubject() {
+                    return this.subject;
+                },
+                get getStartDate() {
+                    return this.startDate;
+                },
+                get getEndDate() {
+                    return this.endDate;
+                },
+                get getMemo() {
+                    return this.memo;
+                }
+            }
+        );
+        this.dayModels[dayID].schedules.push(newSchedule);
+    }
+}
+
+export default ScheduleStore;
